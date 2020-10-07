fb949ce3b025a0c28a58a67a225583139ce26f65
Avutzhan Dautov <dautov92@list.ru>
2020-10-07 17:15:49 +0600

    factorialize number solution 2

--- Basic-Algorithm-Scripting/factorializeNum.js
+++ Basic-Algorithm-Scripting/factorializeNum.js
@@ -1,1 +1,19 @@
+//solution 1
+function factorialize(num) {
+    for (var product = 1; num > 0; num--) {
+        product *= num;
+    }
+    return product;
+}
 
+factorialize(5);
+
+//solution 2
+function factorialize(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorialize(num - 1);
+}
+
+factorialize(5);
