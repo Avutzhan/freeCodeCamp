0743d67a046962e3f80518add22abd3b1de58cff
Avutzhan Dautov <dautov92@list.ru>
2020-10-07 17:24:26 +0600

    factorialize number solution 3

--- Basic-Algorithm-Scripting/factorializeNum.js
+++ Basic-Algorithm-Scripting/factorializeNum.js
@@ -8,12 +8,23 @@ function factorialize(num) {
 
 factorialize(5);
 
-//solution 2
+//solution 2 recursion
 function factorialize(num) {
     if (num === 0) {
         return 1;
     }
     return num * factorialize(num - 1);
+}
+
+factorialize(5);
+
+//solution 3 Tail reqursion
+function factorialize(num, factorial = 1) {
+    if (num == 0) {
+        return factorial;
+    } else {
+        return factorialize(num - 1, factorial * num);
+    }
 }
 
 factorialize(5);
